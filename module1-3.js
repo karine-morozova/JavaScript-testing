2287ebded8df479bbf5f53a2bc68b6fbed321e64
karine-morozova <morozova_karine@ukr.net>
2020-07-25 11:31:20 +0200

    update module1-3.js

---
+++ module1-3.js
@@ -0,0 +1,33 @@
+// Использование if else
+//     Напиши скрипт имитирующий авторизацию администратора в панели управления.
+
+// Есть переменная message в которую надо будет записать одно из трех сообщений о результате:
+
+// const CANCELED_BY_USER = 'Отменено пользователем!'
+// const ACCESS_IS_ALLOWED = 'Добро пожаловать!'
+// const ACCESS_DENIED = 'Доступ запрещен, неверный пароль!
+// Выбор одного из трех значений зависит от того, какое значение будет у переменной userPassword(данные, которые может передать пользователь).Возможны такие варианты:
+
+// const ADMIN_PASSWORD = 'jqueryismyjam'.При таком значении userPassword переменной message следует присвоить ACCESS_IS_ALLOWED.
+// null.При таком значении userPassword переменной message следует присвоить CANCELED_BY_USER.
+// Любая строка, кроме ADMIN_PASSWORD.При таком значении userPassword переменной message следует присвоить ACCESS_DENIED.
+// Используйте if else if else для написания скрипта.
+const userPassword = "jqueryimyjam";
+
+const ADMIN_PASSWORD = "jqueryismyjam";
+
+const CANCELED_BY_USER = "Отменено пользователем!";
+const ACCESS_IS_ALLOWED = "Добро пожаловать!";
+const ACCESS_DENIED = "Доступ запрещен, неверный пароль!";
+let message;
+
+if (userPassword === null) {
+  message = CANCELED_BY_USER;
+  console.log(message);
+} else if (userPassword === ADMIN_PASSWORD) {
+  message = ACCESS_IS_ALLOWED;
+  console.log(message);
+} else {
+  message = ACCESS_DENIED;
+  console.log(message);
+}

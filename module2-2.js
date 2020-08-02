28290b85d9f9a9257638730e16683bd77f640f11
karine-morozova <morozova_karine@ukr.net>
2020-08-02 19:26:11 +0300

    Add module2-2.js

---
+++ module2-2.js
@@ -0,0 +1,17 @@
+// // Напиши скрипт подсчета стоимости гравировки украшений.
+// Для этого создай функцию calculateEngravingPrice(message = "", pricePerWord = 0)
+// принимающую строку(в строке будут только слова и пробелы)
+// и цену гравировки одного слова, и
+// возвращающую цену гравировки всех слов в строке.
+
+//     // Для решения этой задачи не используйте циклы.
+//     Т.е.никаких for, while, do while, for of, for in, forEach
+//     или функциональных методов.
+const calculateEngravingPrice = (message = "", pricePerWord = 0) =>
+  message.split(" ").length * pricePerWord;
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    10
+  )
+);

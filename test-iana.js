e24997478ac42ebfdead64c2606db40c943b753a
karine-morozova <morozova_karine@ukr.net>
2020-08-09 18:53:34 +0300

    Update test-iana.js

--- test-iana.js
+++ test-iana.js
@@ -108,3 +108,36 @@ const sellers = {
 };
 console.log(findBestEmployee(sellers));
 // 'lux'
+
+function getAllPropValues(array, prop) {
+  const propValues = [];
+  for (const element of array) {
+    if (prop in element) {
+      propValues.push(element[prop]);
+    }
+  }
+  console.log(propValues);
+  // if (prop in array) {
+  //   console.log(prop);
+  // }
+}
+
+// Объекты и ожидаемый результат
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Радар", price: 1280, quantity: 2 },
+  { name: "Радар", price: 1320, quantity: 1 },
+  { name: "Сканер", price: 2700, quantity: 1 },
+  { name: "Сканер", price: 2500, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
+
+console.log(getAllPropValues(products, "name"));
+// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, "quantity"));
+// [4, 2, 1, 1, 3, 7, 2]
+
+console.log(getAllPropValues(products, "category"));
+//  []

a5df8a7640bf8596ae819c0eb837d07fde115c36
karine-morozova <morozova_karine@ukr.net>
2020-08-10 23:16:42 +0300

    Add chandes to practice-function-callback.js

--- practice-function-callback.js
+++ practice-function-callback.js
@@ -73,3 +73,73 @@ console.log(freshFruits);
 
 const manyFruits = filter(fruits, (fruit) => fruit.quantity >= 120);
 console.log(manyFruits);
+
+// Lexical Environment
+
+/*
+  Global env - создается при исполнении файла скрипта
+    Record: {}
+    Parent: null
+*/
+
+const x = 10;
+/*
+  Global env
+    Record: {x: 10}
+    Parent: null
+*/
+
+const y = 20;
+/*
+  Global env
+    Record: {x: 10, y: 20}
+    Parent: null
+*/
+
+/*
+  Записывается в момент объявления функции
+  [[Environment]] = Global env
+*/
+const foo = function (z) {
+  /*
+    Foo env - создается в момент вызова функции
+      Record: {z: 30}
+      Parent: Global env
+  */
+
+  const x = 100;
+  /*
+    Foo env - создается в момент вызова функции
+      Record: {z: 30, x: 100}
+      Parent: Global env
+  */
+
+  return x + y + z;
+};
+
+/*
+  Global env
+    Record: {x: 10, y: 20, foo: <function>}
+    Parent: null
+*/
+
+foo(30); // 150
+
+// CLOSURE (замыкание)
+const prepareDish = function (name) {
+  /*
+   * Параметр name это локальная переменная для функции prepareDish.
+   * Это значит что она будет доступна функции cookingDish через замыкание.
+   */
+
+  return function cookingDish(dish) {
+    console.log(`${name} is cooking ${dish}`);
+  };
+};
+const readyDish = prepareDish("Mango");
+readyDish("pizza");
+readyDish("pasta");
+
+const tastyDish = prepareDish("Poly");
+tastyDish("soup");
+tastyDish("steak");
